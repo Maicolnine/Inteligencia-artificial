b2988d02b5abf7b13158e1c378cac6c61a165c33
liljeff1 <jefferson06montano@gmail.com>
2025-07-10 12:18:27 -0500

    modificasiones

--- IA.cpp
+++ IA.cpp
@@ -2,10 +2,20 @@
 #include <string>
 using namespace std;
 
+string responder(const string& entrada) {
+    if (entrada == "hola" || entrada == "Hola") {
+        return "¡Hola! ¿En qué puedo ayudarte?";
+    } else if (entrada == "adios" || entrada == "Adios") {
+        return "¡Hasta luego!";
+    } else {
+        return "No entiendo tu mensaje.";
+    }
+}
 
-
-
-
-int main(){
-cout << "XDDDDD" << endl;
-}// 
+int main() {
+    string mensaje;
+    cout << "Escribe un mensaje: ";
+    getline(cin, mensaje);
+    cout << responder(mensaje) << endl;
+    return 0;
+}
